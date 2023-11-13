2389bde9374a13fbb5cd3d80efc459c4cefb72c6
BRUNOPUMA <108173632+ThulokitoAlarako@users.noreply.github.com>
2023-11-13 16:25:12 -0500

    done

--- JuezCachimbo/src/models/Pregunta.java
+++ JuezCachimbo/src/models/Pregunta.java
@@ -20,4 +20,75 @@ public class Pregunta {
         this.idDocente = idDocente;
         this.alternativa = new Alternativa[4];
     }
+
+    /**
+     * @return the enunciado
+     */
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    /**
+     * @param enunciado the enunciado to set
+     */
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    /**
+     * @return the idTexto
+     */
+    public String getIdTexto() {
+        return idTexto;
+    }
+
+    /**
+     * @param idTexto the idTexto to set
+     */
+    public void setIdTexto(String idTexto) {
+        this.idTexto = idTexto;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the idDocente
+     */
+    public String getIdDocente() {
+        return idDocente;
+    }
+
+    /**
+     * @param idDocente the idDocente to set
+     */
+    public void setIdDocente(String idDocente) {
+        this.idDocente = idDocente;
+    }
+
+    /**
+     * @return the resolucion
+     */
+    public Resolucion getResolucion() {
+        return resolucion;
+    }
+
+    /**
+     * @param resolucion the resolucion to set
+     */
+    public void setResolucion(Resolucion resolucion) {
+        this.resolucion = resolucion;
+    }
+    
 }
